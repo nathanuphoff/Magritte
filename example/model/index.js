3015459786b0600a8293727c4f67b4387b14a1a7
Nathan Uphoff <nathanuphoff@users.noreply.github.com>
2017-02-04 17:36:39 +0100

    Minor text fixes

--- example/model/index.js
+++ example/model/index.js
@@ -1,27 +1,28 @@
 'use strict'
 /*
   # The Model Explained
-  
+  ## Initialising
   Attempting to add anything other than null, a string or number, a boolean, or an Array
   will result in a contentWarning.
 
-  Nested Objects will be used as to the internal data structure, each property will be recursively
-  frozen to a state Object on each render cycle, and transformed to a model dispatcher. Both the 
-  model and state will be available from function components, and event handlers. 
+  Nested Objects will be used to define the data structure, each content-property will be transformed 
+  to a dispatcher and recursively be frozen to a state Object on every render cycle. Both the 
+  model and state will be available from function components and event handlers (automatically). 
   
-  A boolean will be stored as of a boolean-kind and its initial value will be cached, the same
-  concept applies to a string or number which share the ‘content-kind’. By passing ‘null’ to 
-  the model dispatch-method the value will be reset to that of the initial storeModel.
+  ## Content properties
+  A boolean will be stored as a boolean-kind and its initial value will be cached, the same
+  concept applies to a string or number which share the same ‘content-kind’. By passing ‘null’ to 
+  the dispatch-method the state value will be reset to that of the initial storeModel.
   
   An Array is of the Array-kind and its initial ‘null’-value does not encompass the values it holds.
-  If you pass ‘null’ to the dispatch-method of an Array it will be replaced by an new, empty Array.
+  If you pass ‘null’ to the dispatch-method of an Array it will be replaced by a new, empty Array.
   
   The ‘kind of content’ cannot be changed during run-time, it will result in a contentWarning and
   the value dispatched to the state will be ignored. 
   
   Motivation for the store to be as strict:
   - Immutability and fixed kinds of content prevent obscure run-time changes,
-  - This file with its storeModel as seen below will act as the documentation for the entire
+  - This index.js with its storeModel as seen below will act as the documentation for the entire
     data structure of the application which makes it easy to reason about.
   
 */
